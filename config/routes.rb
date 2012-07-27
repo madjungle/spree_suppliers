021b00d00b556c165f8a5884733bec944176045a
Igor Zubkov <icesik@altlinux.org>
2012-07-27 19:39:10 +0300

    Update routes for Rails3

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
-# Put your extension routes here.
-
-map.namespace :admin do |admin|
-  admin.resources :suppliers
-  admin.resources :products do |product|
-    product.resources :suppliers, :member => {:select => :post, :remove => :post}, :collection => {:available => :post, :selected => :get}
-  end
-  admin.resources :orders do |order|
-    order.resources :suppliers, :collection => {:line_items => :get}
+Spree::Core::Engine.routes.prepend do
+  namespace :admin do
+    resources :suppliers
+    resources :products do
+      resources :suppliers, :member => { :select => :post, :remove => :post }, :collection => { :available => :post, :selected => :get }
+    end
+    resources :orders do
+      resources :suppliers, :collection => { :line_items => :get }
+    end
   end
 end
