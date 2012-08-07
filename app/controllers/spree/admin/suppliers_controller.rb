5e7e14e4497a845b78c044f49ff8a83e3753b7e7
Igor Zubkov <icesik@altlinux.org>
2012-08-07 03:57:10 +0300

    Suppliers list and edit works

--- app/controllers/spree/admin/suppliers_controller.rb
+++ app/controllers/spree/admin/suppliers_controller.rb
@@ -1,64 +1,86 @@
 class Spree::Admin::SuppliersController < Spree::Admin::BaseController
-  belongs_to :product
-  before_filter :load_data, :only => [:selected, :available, :remove, :new, :edit, :select]
+#  belongs_to :product
+#  before_filter :load_data, :only => [:selected, :available, :remove, :new, :edit, :select]
 
-  def line_items
-    @order = Order.find_by_number(params[:order_id])
+  before_filter :load_data, :only => [:edit]
+
+  def index
+    @suppliers = Spree::Supplier.find(:all)
   end
 
-  def selected
-    @supplier = @product.supplier
+  def edit
+  	@supplier = Spree::Supplier.find(params[:id])
   end
 
-  def available
-    if params[:q].blank?
-      @available_suppliers = []
-    else
-      @available_suppliers = Supplier.find(:all, :conditions => ['lower(name) LIKE ?', "%#{params[:q].downcase}%"])
-    end
-    @available_suppliers.delete_if { |supplier| @product.supplier == supplier }
+  def update
+  	@supplier = Spree::Supplier.find(params[:id])
+
     respond_to do |format|
-      format.html
-      format.js { render :layout => false }
+      if @supplier.update_attributes(params[:supplier])
+        format.html { redirect_to admin_suppliers_path }
+      else
+        format.html { render :action => 'edit' }
+      end
     end
   end
 
-  def remove
-    @product.supplier = nil
-    @product.save
-    @supplier = @product.supplier
-    render :layout => false
-  end
-
-  def select
-    @supplier = Supplier.find_by_param!(params[:id])
-    @product.supplier = @supplier
-    @product.save
-    @supplier = @product.supplier
-    render :layout => false
-  end
-
-  update.response do |wants|
-    wants.html { redirect_to collection_url }
-  end
-
-  update.after do
-    Rails.cache.delete('suppliers')
-  end
-
-  create.response do |wants|
-    wants.html { redirect_to collection_url }
-  end
-
-  create.after do
-    Rails.cache.delete('suppliers')
-  end
+#  def line_items
+#    @order = Order.find_by_number(params[:order_id])
+#  end
+#
+#  def selected
+#    @supplier = @product.supplier
+#  end
+#
+#  def available
+#    if params[:q].blank?
+#      @available_suppliers = []
+#    else
+#      @available_suppliers = Supplier.find(:all, :conditions => ['lower(name) LIKE ?', "%#{params[:q].downcase}%"])
+#    end
+#    @available_suppliers.delete_if { |supplier| @product.supplier == supplier }
+#    respond_to do |format|
+#      format.html
+#      format.js { render :layout => false }
+#    end
+#  end
+#
+#  def remove
+#    @product.supplier = nil
+#    @product.save
+#    @supplier = @product.supplier
+#    render :layout => false
+#  end
+#
+#  def select
+#    @supplier = Supplier.find_by_param!(params[:id])
+#    @product.supplier = @supplier
+#    @product.save
+#    @supplier = @product.supplier
+#    render :layout => false
+#  end
+#
+#  update.response do |wants|
+#    wants.html { redirect_to collection_url }
+#  end
+#
+#  update.after do
+#    Rails.cache.delete('suppliers')
+#  end
+#
+#  create.response do |wants|
+#    wants.html { redirect_to collection_url }
+#  end
+#
+#  create.after do
+#    Rails.cache.delete('suppliers')
+#  end
 
   private
 
   def load_data
-    default_country = Country.find Spree::Config[:default_country_id]
-    @states = default_country.states.sort
-    @product = Product.find_by_permalink(params[:product_id])
+   default_country = Spree::Country.find Spree::Config[:default_country_id]
+   @states = default_country.states.sort
+   @product = Spree::Product.find_by_permalink(params[:product_id])
   end
 end
--- app/models/spree/supplier.rb
+++ app/models/spree/supplier.rb
@@ -1,4 +1,6 @@
 class Spree::Supplier < ActiveRecord::Base
+  attr_accessible :name, :address1, :address2, :city, :state_id, :zip_code, :phone, :fax, :email, :website, :notes
+
   belongs_to :state, :class_name => "Spree::State"
   has_many :products
 end
