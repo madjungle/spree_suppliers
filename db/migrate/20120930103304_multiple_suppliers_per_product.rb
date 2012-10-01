5f46de55ab458dc2c8688db2a46db2f7a18ffbdd
Alex Dowad <alexinbeijing@gmail.com>
2012-10-01 15:31:19 +0200

    Make multiple_suppliers_per_product migration script reversible

--- db/migrate/20120930103304_multiple_suppliers_per_product.rb
+++ db/migrate/20120930103304_multiple_suppliers_per_product.rb
@@ -1,5 +1,5 @@
 class MultipleSuppliersPerProduct < ActiveRecord::Migration
-  def change
+  def self.up
     remove_column :spree_products, :supplier_id
     create_table :products_suppliers do |t|
       t.integer :product_id
@@ -7,5 +7,9 @@ class MultipleSuppliersPerProduct < ActiveRecord::Migration
     end
     add_index :products_suppliers, :product_id
     add_index :products_suppliers, :supplier_id
+  end
+  def self.down
+    add_column :spree_products, :supplier_id
+    drop_table :products_suppliers
   end
 end
