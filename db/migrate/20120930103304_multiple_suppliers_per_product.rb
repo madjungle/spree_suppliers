5863fd256f6f2cc62290e2becf13a620a609de65
Sam Hamilton <sam@sh81.com>
2012-10-01 10:21:21 +0800

    Revert "correct table name from products_suppliers to spree_products_suppliers"
    This reverts commit f40e4fd6d230556020b475d5b6b4e927285e6076.

--- db/migrate/20120930103304_multiple_suppliers_per_product.rb
+++ db/migrate/20120930103304_multiple_suppliers_per_product.rb
@@ -1,7 +1,7 @@
 class MultipleSuppliersPerProduct < ActiveRecord::Migration
   def change
     remove_column :spree_products, :supplier_id
-    create_table :spree_products_suppliers, :id => false do |t|
+    create_table :products_suppliers, :id => false do |t|
       t.integer :product_id
       t.integer :supplier_id
     end
