1fd8cba713d84ef69fe928f6bc2eed65eeea9f17
Alex Dowad <alexinbeijing@gmail.com>
2012-10-03 16:37:26 +0200

    Set table name for Spree::Supplier manually

--- app/models/spree/supplier.rb
+++ app/models/spree/supplier.rb
@@ -1,4 +1,5 @@
 class Spree::Supplier < ActiveRecord::Base
+  set_table_name "suppliers"
   attr_accessible :name, :address1, :address2, :city, :state_id, :zip_code, :phone, :fax, :email, :website, :notes
 
   belongs_to :state, :class_name => "Spree::State"
