5af2d49b43aa5ed7460371aff8b136ba9fd06a2e
Alex Dowad <alexinbeijing@gmail.com>
2012-10-04 14:51:09 +0200

    set_table_name in Spree::Supplier was not actually necessary

--- app/models/spree/supplier.rb
+++ app/models/spree/supplier.rb
@@ -1,5 +1,4 @@
 class Spree::Supplier < ActiveRecord::Base
-  set_table_name "suppliers"
   attr_accessible :name, :address1, :address2, :city, :state_id, :zip_code, :phone, :fax, :email, :website, :notes
 
   belongs_to :state, :class_name => "Spree::State"
