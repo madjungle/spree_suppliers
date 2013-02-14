364d5d553f2aa02d22effc160a2c340120858f62
Igor Zubkov <icesik@altlinux.org>
2013-02-14 18:18:46 +0200

    Fix suppliers show on admin product edit page

--- app/overrides/add_suppliers_to_admin_products.rb
+++ app/overrides/add_suppliers_to_admin_products.rb
@@ -2,7 +2,8 @@ Deface::Override.new(:virtual_path => "spree/admin/shared/_product_tabs",
                      :name => 'add_suppliers_to_admin_products',
                      :insert_bottom => "[data-hook='admin_product_tabs'], #admin_product_tabs[data-hook]",
                      :text => "<li<%== ' class=\"active\"' if current == 'Suppliers' %>>
-                                 <%= link_to t(:suppliers), selected_admin_product_suppliers_path(@product) %>
+                                 <%= link_to_with_icon 'icon-group', t(:suppliers),
+                                     selected_admin_product_suppliers_path(@product) %>
                               </li>",
                      :original => '3801430748a019ba9c003cdf356289c7f9d98724',
                      :disabled => false)
