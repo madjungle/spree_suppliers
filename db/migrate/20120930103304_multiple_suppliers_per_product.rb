c044e6c23e1a9cfa9004b65cf728790a7f537c32
Igor Zubkov <icesik@altlinux.org>
2013-02-14 18:02:30 +0200

    Cleanup

--- db/migrate/20120930103304_multiple_suppliers_per_product.rb
+++ db/migrate/20120930103304_multiple_suppliers_per_product.rb
@@ -1,5 +1,5 @@
 class MultipleSuppliersPerProduct < ActiveRecord::Migration
-  def self.up
+  def up
     remove_column :spree_products, :supplier_id
     create_table :products_suppliers do |t|
       t.integer :product_id
@@ -8,7 +8,8 @@ class MultipleSuppliersPerProduct < ActiveRecord::Migration
     add_index :products_suppliers, :product_id
     add_index :products_suppliers, :supplier_id
   end
-  def self.down
+
+  def down
     add_column :spree_products, :supplier_id
     drop_table :products_suppliers
   end
